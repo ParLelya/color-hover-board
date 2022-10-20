87fcedaae24592f262053dc16411de686c8244ca
Olga <parlelya@vk.com>
2022-10-20 19:18:58 +0300

    Add color randomizer

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const board = document.querySelector("#board");
-const colors = [
-  "#A52A2A",
-  "#D2691E",
-  "#9370DB",
-  "#4B0082",
-  "#4682B4",
-  "#808000",
-  "#B22222",
-  "#7CFC00",
-  "#008B8B",
-  "#FF69B4",
-];
+// const colors = [
+//   "#A52A2A",
+//   "#D2691E",
+//   "#9370DB",
+//   "#4B0082",
+//   "#4682B4",
+//   "#808000",
+//   "#B22222",
+//   "#7CFC00",
+//   "#008B8B",
+//   "#FF69B4",
+// ];
 const SQUARES_NUMBER = 726;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
@@ -24,7 +24,8 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 }
 
 function setColor(element) {
-  const color = getColor();
+  //   const color = getColor();
+  const color = generateColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
@@ -34,7 +35,16 @@ function removeColor(element) {
   element.style.boxShadow = `0 0 2px #000`;
 }
 
-function getColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+// function getColor() {
+//   const index = Math.floor(Math.random() * colors.length);
+//   return colors[index];
+// }
+
+function generateColor() {
+  const hexSymbols = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexSymbols[Math.floor(Math.random() * hexSymbols.length)];
+  }
+  return "#" + color;
 }
