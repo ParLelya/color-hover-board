673ee2f4fe4d80b7577330c11c054e46af19fc78
Olga <parlelya@vk.com>
2022-10-21 15:50:16 +0300

    Small fix

--- script.js
+++ script.js
@@ -17,20 +17,22 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => setColor(square));
-  square.addEventListener("mouseleave", () => removeColor(square));
+  square.addEventListener("mouseover", setColor);
+  square.addEventListener("mouseleave", removeColor);
 
   board.append(square);
 }
 
-function setColor(element) {
+function setColor(event) {
   //   const color = getColor();
+  const element = event.target;
   const color = generateColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target;
   element.style.backgroundColor = "#474747";
   element.style.boxShadow = `0 0 2px #000`;
 }
